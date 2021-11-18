565f2747241bbf4346a7533732352a81c460abd4
Piotr Karon <piotr.karon@millionpugs.com>
2021-11-18 22:09:31 +0100

    Make input a list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinFeature
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
@@ -8,19 +5,6 @@ import kotlinx.cli.required
 import java.io.File
 import java.lang.Exception
 import kotlin.time.Duration.Companion.milliseconds
-
-val jsonMapper = ObjectMapper().apply {
-    registerModule(
-        KotlinModule.Builder()
-            .withReflectionCacheSize(512)
-            .configure(KotlinFeature.NullToEmptyCollection, false)
-            .configure(KotlinFeature.NullToEmptyMap, false)
-            .configure(KotlinFeature.NullIsSameAsDefault, false)
-            .configure(KotlinFeature.SingletonSupport, false)
-            .configure(KotlinFeature.StrictNullChecks, false)
-            .build()
-    )
-}
 
 enum class Algorithm {
     REF, GEN
@@ -38,15 +22,20 @@ fun main(args: Array<String>) {
         description = "Algorithm: ref - reference, gen - genetic"
     ).required()
 
-    val weight by parser.option(ArgType.Int, shortName = "w", description = "Weight value override")
-
     parser.parse(args)
 
     try {
-        val problem = DataLoader.load(input)
+        val problems = DataLoader.load(input)
+        println("Loaded ${problems.size} problem instances")
 
-        println(problem)
-    }catch (e: Exception) {
+        val results = problems.map {
+            println("Solving problems using $algorithm algorithm")
+
+            object : ProblemSolver {}.solve(it).also { println("Resolved. Result: \n$it") }
+        }
+
+    } catch (e: Exception) {
+        e.printStackTrace()
         println("There was error while running the program: ${e.message}.")
         println("Exiting..")
     }
@@ -54,48 +43,86 @@ fun main(args: Array<String>) {
 
 object DataLoader {
 
-    fun load(path: String): Problem {
+    fun load(path: String): List<Problem> {
         val file = File(path)
-        require(file.exists()) { "File $path doesn't exist" }
+        require(file.isDirectory) { "Given input path $path must exist and be directory" }
+        require(requireNotNull(file.listFiles()).isNotEmpty()) { "Input directory must not be empty" }
 
-        val lines = file.readLines()
-        require(lines.isNotEmpty()) { "File is empty" }
+        val problems = file.listFiles()?.map {
 
-        val weightStr = requireNotNull(lines.firstOrNull()?.split(",")?.firstOrNull()) { "Weight not present" }
-        val weight = requireNotNull(weightStr.toIntOrNull()) { "Couldn't parse weight $weightStr to Int" }
-        require(weight > 0) { "Weight must be greater than 0" }
+            val lines = it.readLines()
+            require(lines.isNotEmpty()) { "File is empty" }
 
-        val items = lines.drop(1).mapIndexed { idx, it ->
-            val lineNumber = idx + 1
-            val line = it.split(",")
-            Item(
-                name = requireNotNull(line.getOrNull(0)) { "Name must not be null. Line $lineNumber"},
-                weight = requirePositiveInt(requireNotNull(line.getOrNull(1)){"Weight must not be null"}, "Weight", lineNumber),
-                value = requirePositiveInt(requireNotNull(line.getOrNull(2)){"Value must not be null"}, "Value", lineNumber),
-                number = lineNumber
+            val weightStr = requireNotNull(lines.firstOrNull()?.split(",")?.firstOrNull()) { "Weight not present" }
+            val weight = requireNotNull(weightStr.toIntOrNull()) { "Couldn't parse weight $weightStr to Int" }
+            require(weight > 0) { "Weight must be greater than 0" }
+
+            val items = lines.drop(1).mapIndexed { idx, it ->
+                val lineNumber = idx + 1
+                val line = it.split(",")
+                Item(
+                    name = requireNotNull(line.getOrNull(0)) { "Name must not be null. Line $lineNumber" },
+                    weight = requirePositiveInt(
+                        requireNotNull(line.getOrNull(1)) { "Weight must not be null" },
+                        "Weight",
+                        lineNumber
+                    ),
+                    value = requirePositiveInt(
+                        requireNotNull(line.getOrNull(2)) { "Value must not be null" },
+                        "Value",
+                        lineNumber
+                    ),
+                    number = lineNumber
+                )
+            }
+
+            Problem(
+                weightLimit = weight,
+                items = items.toSet(),
+                fileName = it.name
             )
         }
 
-        return Problem(
-            weightLimit = weight,
-            items = items.toSet()
-        )
+        return problems ?: throw IllegalStateException("There was error reading files")
     }
+}
+
+interface ProblemSolver {
+    fun solve(problem: Problem): ProblemResult = ProblemResult(1000 * 3 * 62, 30, 29, 300, Algorithm.GEN, "data1.csv")
 }
 
 data class ProblemResult(
     val timeMillis: Long,
+    val weightLimit: Int,
     val weight: Int,
     val value: Int,
-    val algorithm: Algorithm
+    val algorithm: Algorithm,
+    val inputFileName: String
 ) {
     val time = timeMillis.milliseconds.toString()
+    val outputFileName = "resolved-$inputFileName"
+
+    override fun toString(): String {
+        return """
+            time: $time (${timeMillis}ms)
+            weightLimit: $weightLimit
+            weight: $weight
+            value: $value
+            algorithm: $algorithm
+            output file: $outputFileName
+        """.trimIndent()
+    }
+
+    fun csv() = "$timeMillis,$weightLimit,$weight,$value,$algorithm,$outputFileName"
 }
 
 data class Problem(
     val weightLimit: Int,
-    val items: Set<Item>
-)
+    val items: Set<Item>,
+    val fileName: String
+) {
+    fun overrideWeight(weightLimit: Int) = this.copy(weightLimit = weightLimit)
+}
 
 data class Item(
     val name: String,
@@ -111,7 +138,7 @@ data class Item(
 }
 
 const val inputDescription = """
-    Input file path.
+    Path to a directory containing files with problem data.
 			File should follow this format:
 				30,,       – first line contains weight limit
 				Gold,1,200 – next lines contain Item name,Weight,Value
@@ -120,12 +147,12 @@ const val inputDescription = """
 
 fun requirePositiveInt(string: String, fieldName: String, lineNumber: Int? = null): Int {
     val value = string.toIntOrNull()
-    require(value != null) { "$fieldName (value=$string) must be an Int. ${lineNumber?.let { "Line $it" }}"}
+    require(value != null) { "$fieldName (value=$string) must be an Int. ${lineNumber?.let { "Line $it" }}" }
 
     return requirePositiveInt(value, fieldName, lineNumber)
 }
 
 fun requirePositiveInt(value: Int, fieldName: String, lineNumber: Int? = null): Int {
-    require(value > 0) {"$fieldName (value=$value) must be greater than 0. ${lineNumber?.let { "Line $it" }}"}
+    require(value > 0) { "$fieldName (value=$value) must be greater than 0. ${lineNumber?.let { "Line $it" }}" }
     return value
 }
