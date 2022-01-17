7fc0eb916e868f84ddee094961ba05cc1592b749
Piotr Karon <piotr.karon@millionpugs.com>
2022-01-17 22:33:05 +0100

    Revert main

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -7,17 +7,12 @@ import algorithm.genetic.crossover.ScoreBasedCrossover
 import algorithm.genetic.generator.RandomGenesisPopulationGenerator
 import algorithm.genetic.mutator.RandomMutator
 import algorithm.genetic.selector.TournamentSelector
-import app.tuning.InputParams
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
 import kotlinx.cli.default
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.runBlocking
 import java.lang.Exception
-import java.time.Instant
-import kotlin.math.roundToInt
 
 private var enableDebug = true
 
@@ -27,11 +22,10 @@ fun debugScope(fn: () -> Unit) {
 
 @OptIn(ExperimentalCli::class)
 fun main(args: Array<String>) {
-    runTuning()
-//    val parser = ArgParser("aoin")
-//
-//    parser.subcommands(Reference(), Genetic())
-//    parser.parse(args)
+    val parser = ArgParser("aoin")
+
+    parser.subcommands(Reference(), Genetic())
+    parser.parse(args)
 }
 
 @OptIn(ExperimentalCli::class)
@@ -100,64 +94,4 @@ class Genetic : AlgorithmSubcommand("genetic", "Run genetic algorithm") {
 class Reference : AlgorithmSubcommand("reference", "Run reference algorithm - dynamic programming") {
     override val algorithmDetails: AlgorithmDetails
         get() = AlgorithmDetails.DynamicProgramming
-}
-
-fun runTuning(): Unit  {
-    val input = "./data"
-    val output = "./tuning"
-    val fileName = "tuning-${Instant.now()}"
-    val repeats = 1
-
-    val problems = DataLoader.load(input)
-    val threads = mutableListOf<Job>()
-
-    problems.forEachIndexed { idx, problem ->
-        println("${problem.fileName}: items: ${problem.items.size}")
-        gen().map { params ->
-            repeat(repeats) { repeat ->
-//                    threads += launch {
-                val res = ProblemSolver.solve(
-                    problem,
-                    AlgorithmDetails.Genetic(
-                        GeneticAlgorithmParameters(
-                            numberOfGenerations = params.numberOfGenerations,
-                            genesisPopulationGenerator =
-                            RandomGenesisPopulationGenerator(
-                                populationSize = params.genesisPopulationSize.coerceAtMost((problem.items.size * 0.8).roundToInt())
-                            ),
-                            selector = TournamentSelector(params.tournamentSize, 2),
-                            crossover = ScoreBasedCrossover,
-                            mutator = RandomMutator(params.mutationRate)
-                        )
-                    )
-                )
-                println("$repeat ${problem.fileName} with input params: $params | time=${res.timeMillis} %opt=${(res.value.toDouble() / (res.expectedOptimum ?: 1) * 100.0).roundToInt()}")
-                ResultSaver.saveTuning(res, params, output, fileName + "$idx$repeat")
-
-//                    }
-            }
-        }
-//        threads.forEach { it.start() }
-//        threads.forEach { it.join() }
-    }
-}
-
-// Fixed values
-// genesis population size depend on size of instance
-// generations depend
-private val mutationRates = listOf(0.05, 0.1)
-private val generationSizes = listOf(50, 100, 150, 200, 250, 300, 400, 500)
-private val genesisSize = listOf(10, 30, 50, 100)
-
-fun gen() = mutationRates.flatMap { mutationRates ->
-    generationSizes.flatMap { genNum ->
-        genesisSize.map { genStartSize ->
-            InputParams(
-                numberOfGenerations = genNum,
-                genesisPopulationSize = genStartSize,
-                tournamentSize = 2,
-                mutationRate = mutationRates,
-            )
-        }
-    }
 }
